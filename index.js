39473bb87ec1a29f49282e9d4930faca8c722c6d
rdnanuar <rdjanuar008@gmail.com>
2022-06-29 03:31:32 +0700

    .

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const write = require("./utils")
 const app = express()
-const PORT = process.env.PORT || 8080
-
+const {v4 : uuid} = require("uuid")
 const friends = require("./data/friends.json")
+const { response } = require("express")
+
+const PORT = process.env.PORT || 8080
 
 app.use((req, res, next) => {
     const start = Date.now();
@@ -16,13 +18,30 @@ app.use(express.json())
 
 app.post('/friends', (req, res) => {
     const newFriend = {
-        id : friends.length,
+        id : uuid(),
         name : req.body.name,
 
     }
     friends.push(newFriend)
     write("./data/friends.json", friends)
-    res.send(newFriend)
+    res.status(201).json({
+        data : newFriend
+    })
+})
+
+app.delete("/friends/:id", (req, res) => {
+    const friendId = friends.find(({id}) => id === String(req.params.id))
+    if(!friendId) {
+        res.status(404).json({
+            message : `Data with id ${req.params.id} not found`
+        })
+    }else {
+        friends.splice(friendId, 1)
+        write("./data/friends.json", friends)
+        res.status(200).json({
+            message : `Data with id ${req.params.id} has been deleted`
+        })
+    }
 })
 
 app.get("/friends", (req, res) => {
@@ -33,7 +52,7 @@ app.get("/friends", (req, res) => {
 
 app.get("/friends/:id", (req, res) => {
     
-    const friendId = friends.find(({id}) =>id === Number(req.params.id))
+    const friendId = friends.find(({id}) => id === Number(req.params.id))
 
     return friendId ?  res.status(200).json({data : [friendId]}) : res.status(404).json({message : `friend with id ${req.params.id} not found`})
 })
