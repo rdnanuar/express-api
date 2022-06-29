977f1123bd2851c7e0496dcee893e1600599b639
rdnanuar <rdjanuar008@gmail.com>
2022-06-29 22:23:11 +0700

    .

--- src/controllers/friends.js
+++ src/controllers/friends.js
@@ -5,17 +5,33 @@ const {v4 : uuid} = require("uuid")
 
 const listData = {
 
+    /**
+     * @desc get all data from firends 
+     */
+
     getFriendsData(req, res) {
         res.status(200).json({
         data : friends
     })
     },
 
+    /**
+     * 
+     * @param {req.params.id} req request to express param 
+     * @param {response} res response to client
+     * @returns {friendId} return one friend with selected ID
+     */
+
     getFriendsWithId(req, res) {
         const friendId = friends.find(({id}) => id === req.params.id)
 
         return friendId ?  res.status(200).json({data : [friendId]}) : res.status(404).json({message : `friend with id ${req.params.id} not found`})
     },
+    /**
+     * 
+     * @param {req.body.name} req request to express body json 
+     * @param {res.data} res  response to client with data has been created
+     */
 
     createFriends(req, res) {
         if(!req.body.name) {
@@ -36,6 +52,12 @@ const listData = {
         }
     },
 
+    /**
+     * 
+     * @param {id, name} req request body name and params id from express
+     * @param {friendId} res response to client with data has been updated
+     */
+
     updateFriend(req, res) {
         const friendId = friends.findIndex(p => p.id === req.params.id)
         
@@ -49,6 +71,11 @@ const listData = {
             data : updateFriend
         })
     },
+    /**
+     * 
+     * @param {req.params.id} req request params id from express 
+     * @param {friendId} res respsonse to client with message data has been deleted 
+     */
 
     deleteFriend(req, res) {
         const friendId = friends.filter(p => p.id !== req.params.id)
